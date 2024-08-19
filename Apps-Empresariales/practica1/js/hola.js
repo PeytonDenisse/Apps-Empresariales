d30bc27bf5e5b9f6170db8b7878b1f27ea073738
PeytonDenisse <dcv78658@lasalle.edu.mx>
2024-08-19 09:12:43 -0600

    primer commit

---
+++ Apps-Empresariales/practica1/js/hola.js
@@ -0,0 +1,5 @@
+var mensaje = "Hola TS";
+console.log(mensaje);
+//var ley y cosnt
+var numero = 10;
+console.log(numero)
---
+++ Apps-Empresariales/practica1/src/hola.ts
@@ -0,0 +1,9 @@
+let mensaje: string = "Hola TS"
+console.log(mensaje);
+
+
+//var ley y cosnt
+
+let numero:number = 10;
+numero = 555;
+console.log(numero);
