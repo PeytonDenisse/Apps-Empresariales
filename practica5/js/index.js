ba8291cd02387570225e9c8bc9b70f273812febd
PeytonDenisse <dcv78658@lasalle.edu.mx>
2024-09-10 08:13:58 -0600

    herencia

--- practica5/js/index.js
+++ practica5/js/index.js
@@ -1,3 +1,18 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var variable1 = 'Hola Mundo';
 console.log(variable1);
 //crear persona 
@@ -19,8 +34,10 @@ var frutaA = frutas[0], frutaB = frutas[3];
 console.log(frutaA, frutaB);
 //creando una clase 
 var Persona = /** @class */ (function () {
-    function Persona(nombre, edad) {
+    function Persona(nombre, apellidoPaterno, apellidoMaterno, edad) {
         this.nombre = nombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
         this.edad = edad;
     }
     //metodos 
@@ -37,9 +54,25 @@ var Persona = /** @class */ (function () {
     Persona.prototype.setNombre = function (nombre) {
         this.nombre = nombre;
     };
+    Persona.prototype.setApellidoPaterno = function () {
+        return this.apellidoPaterno;
+    };
+    Persona.prototype.setapellidoMaterno = function () {
+        return this.apellidoMaterno = this.apellidoPaterno;
+    };
+    Persona.prototype.getApellidoPaterno = function () {
+        return this.apellidoPaterno;
+    };
+    Persona.prototype.getApellidoMaterno = function () {
+        return this.apellidoMaterno;
+    };
+    Persona.prototype.getNomnreCompleto = function () {
+        return this.getNombre + '' + this.apellidoPaterno + '' + this.apellidoMaterno;
+    };
     return Persona;
 }());
-var personaje1 = new Persona('Alberto', 21);
+// crear nuaeva persona 
+var personaje1 = new Persona('Alberto', 'Guadarrama', 'Delgado', 21);
 //imprimir de diferentes maneras 
 console.log(personaje1.nombre, personaje1.edad);
 console.log(personaje1.nombre);
@@ -47,6 +80,78 @@ console.log(personaje1.edad);
 //cambiar la variable 
 personaje1.edad = 25;
 personaje1.nombre = 'Edwin';
-var personaje2 = new Persona('Francisco', 19);
+var personaje2 = new Persona('Edwin', 'Rodriguez', 'Martinez', 19);
 personaje2.setEdad(20);
 personaje2.setNombre('Nombre:' + 'Juan');
+console.log(personaje2.getNombre());
+console.log(personaje2.getNomnreCompleto());
+var UserAccount = /** @class */ (function () {
+    function UserAccount(name, id) {
+        this.name = name;
+        this.id = id;
+    }
+    return UserAccount;
+}());
+var usuarioX = new UserAccount('Imagine Dragons', 1);
+console.log(usuarioX.name, usuarioX.id);
+//crear al menos 5 clases que se van a usar en el proyecto final
+/*class Producto{
+    id: number;
+    name: string;
+    precio: number;
+
+    constructor(id: number, name: )
+
+}*/
+var Animal = /** @class */ (function () {
+    function Animal(nombre) {
+        this.nombre = nombre;
+    }
+    Animal.prototype.moverse = function (distancia) {
+        if (distancia === void 0) { distancia = 0; }
+        console.log("".concat(this.nombre, " se movio ").concat(distancia, "mts"));
+    };
+    Animal.prototype.hacerSonido = function (sonido) {
+        if (sonido === void 0) { sonido = 'Sin sonido'; }
+        console.log("".concat(this.nombre, " hace ").concat(sonido));
+    };
+    return Animal;
+}());
+var animal1 = new Animal('Grillo');
+animal1.moverse(5);
+animal1.hacerSonido('Cri Cri');
+var Perro = /** @class */ (function (_super) {
+    __extends(Perro, _super);
+    function Perro(nombre) {
+        return _super.call(this, nombre) || this;
+    }
+    Perro.prototype.moverse = function (distancia) {
+        if (distancia === void 0) { distancia = 5; }
+        _super.prototype.moverse.call(this, distancia);
+    };
+    Perro.prototype.hacerSonido = function (sonido) {
+        if (sonido === void 0) { sonido = 'Ladrido'; }
+        console.log("".concat(this.nombre, " hace ").concat(sonido));
+    };
+    return Perro;
+}(Animal));
+var perro1 = new Perro('Firulais');
+perro1.moverse(10);
+perro1.hacerSonido('Miau Miau');
+var Serpiente = /** @class */ (function () {
+    function Serpiente(nombre) {
+        this.nombre = nombre;
+    }
+    Serpiente.prototype.moverse = function (distancia) {
+        if (distancia === void 0) { distancia = 0; }
+        console.log("".concat(this.nombre, " se movio ").concat(distancia, "mts"));
+    };
+    Serpiente.prototype.hacerSonido = function (sonido) {
+        if (sonido === void 0) { sonido = 'Siseo'; }
+        console.log("".concat(this.nombre, " hace ").concat(sonido));
+    };
+    return Serpiente;
+}());
+var serpiente1 = new Serpiente('Cobra');
+serpiente1.moverse(2);
+serpiente1.hacerSonido('Siseo');
--- practica5/src/index.ts
+++ practica5/src/index.ts
@@ -26,9 +26,14 @@ console.log(frutaA,frutaB);
 //creando una clase 
 class Persona{
     nombre: string;
+    apellidoPaterno: string;
+    apellidoMaterno: string;
     edad: number;
-    constructor(nombre: string, edad: number){
+
+    constructor(nombre: string,  apellidoPaterno: string, apellidoMaterno:string, edad: number){
         this.nombre = nombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
         this.edad = edad;
     }
     //metodos 
@@ -46,11 +51,32 @@ class Persona{
     setNombre(nombre: string):void {
         this.nombre = nombre;
         }
+    
+    setApellidoPaterno(): string{
+            return this.apellidoPaterno;
+        }
+
+    setapellidoMaterno(): string{
+        return this.apellidoMaterno = this.apellidoPaterno;
+    }
+
+    getApellidoPaterno():string{
+        return this.apellidoPaterno;
+    }
+
+    getApellidoMaterno():string{
+        return this.apellidoMaterno;
+    }
+
+    getNomnreCompleto():string{
+        return this.getNombre +''+this.apellidoPaterno+''+this.apellidoMaterno
+    }
+
 }
 
 
-
-let personaje1 = new Persona('Alberto', 21);
+// crear nuaeva persona 
+let personaje1 = new Persona('Alberto', 'Guadarrama','Delgado',21);
 //imprimir de diferentes maneras 
 console.log(personaje1.nombre, personaje1.edad);
 console.log(personaje1.nombre);
@@ -60,6 +86,97 @@ console.log(personaje1.edad);
 personaje1.edad= 25;
 personaje1.nombre = 'Edwin';
 
-let personaje2 = new Persona('Francisco',19);
+let personaje2 = new Persona('Edwin','Rodriguez','Martinez',19);
 personaje2.setEdad(20);
 personaje2.setNombre('Nombre:'+'Juan');
+
+console.log(personaje2.getNombre());
+console.log(personaje2.getNomnreCompleto());
+
+
+//interface
+interface User{
+    name: string;
+    id: number;
+
+}
+
+class UserAccount{
+    name: string;
+    id: number;
+
+    constructor(name: string, id: number){
+        this.name = name;
+        this.id = id;
+    }
+
+}
+
+
+const usuarioX: User = new UserAccount('Imagine Dragons', 1);
+console.log(usuarioX.name, usuarioX.id);
+
+//crear al menos 5 clases que se van a usar en el proyecto final
+
+
+/*class Producto{
+    id: number;
+    name: string;
+    precio: number;
+
+    constructor(id: number, name: )
+
+}*/
+
+class Animal {
+    nombre: string;
+    constructor(nombre: string){
+        this.nombre = nombre;
+    }
+    moverse(distancia: number = 0){
+        console.log(`${this.nombre} se movio ${distancia}mts`);
+    }
+    hacerSonido(sonido: string = 'Sin sonido'){
+        console.log(`${this.nombre} hace ${sonido}`);
+    }
+
+}
+
+let animal1 = new Animal('Grillo');
+animal1.moverse(5);
+animal1.hacerSonido('Cri Cri');
+
+
+class Perro extends Animal{
+    constructor(nombre: string){
+        super(nombre);
+    }
+    moverse(distancia: number = 5){
+        super.moverse(distancia);
+    }
+    hacerSonido(sonido: string = 'Ladrido'){
+        console.log(`${this.nombre} hace ${sonido}`);
+    }
+}
+
+let perro1 = new Perro('Firulais');
+perro1.moverse(10);
+perro1.hacerSonido('Miau Miau');
+
+
+class Serpiente{
+    nombre: string;
+    constructor(nombre: string){
+        this.nombre = nombre;
+    }
+    moverse(distancia: number = 0){
+        console.log(`${this.nombre} se movio ${distancia}mts`);
+    }
+    hacerSonido(sonido: string = 'Siseo'){
+        console.log(`${this.nombre} hace ${sonido}`);
+    }
+}
+
+let serpiente1 = new Serpiente('Cobra');
+serpiente1.moverse(2);
+serpiente1.hacerSonido('Siseo');
