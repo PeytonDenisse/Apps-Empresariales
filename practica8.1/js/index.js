7a4a8f21c26b4c9047e8050ea182b93d5f37e475
PeytonDenisse <dcv78658@lasalle.edu.mx>
2024-09-24 08:29:02 -0600

    practica8.1

--- practica8.1/js/index.js
+++ practica8.1/js/index.js
@@ -1,2 +1,42 @@
 "use strict";
 console.log("Hola CodeSpaces");
+const nuevoHeroe = "Loky";
+function returnName() {
+    return nuevoHeroe;
+}
+const heroeNombre = returnName();
+let otroNombre = returnName();
+console.log("De que tipo es", typeof otroNombre);
+//parametros obligatorios en funciones 
+const nombreCompleto = (nombre, apellido) => {
+    return `${nombre} ${apellido}`;
+};
+const nombre = nombreCompleto("Peyton", "Manning");
+console.log(nombre);
+//parametros opcionales en funciones SWIF
+const nombreCompleto2 = (nombre, apellido) => {
+    return `${nombre} ${apellido || 'sin apellido'}`;
+};
+const nombre2 = nombreCompleto2("Peyton");
+console.log(nombre2);
+//parametros por defecto en funciones
+const nombreCompleto3 = (nombre, apellido, mayusculas = false) => {
+    if (mayusculas) {
+        return `${nombre || 'sin nombre'} ${apellido || 'sin apellido'}`.toUpperCase();
+    }
+    else {
+        return `${nombre || 'sin nombre'} ${apellido || 'sin apellido'}`;
+    }
+};
+const nombre3 = nombreCompleto3("Peyton", "Manning", true);
+console.log(nombre3);
+//esta funcion en ternario 
+const nombreCompleto4 = (nombre, apellido, mayusculas = false) => (mayusculas) ? `${nombre || 'sin nombre'} ${apellido || 'sin apellido'}`.toUpperCase() : `${nombre || 'sin nombre'} ${apellido || 'sin apellido'}`;
+const nombre4 = nombreCompleto4("Peyton", "Manning", true);
+console.log(nombre4);
+//operador Rest
+const nombreCompleto5 = (nombre, ...restParametros) => {
+    return `${nombre} ${restParametros.join(' ')}`;
+};
+const nombre5 = nombreCompleto5("Peyton", "Manning", "Veloz");
+console.log(nombre5);
--- practica8.1/src/index.js
+++ practica8.1/src/index.js
@@ -1,56 +1,42 @@
+"use strict";
 console.log("Hola CodeSpaces");
-
-const nuevoHeroe:string = "Loky";
-function returnName():string {
+const nuevoHeroe = "Loky";
+function returnName() {
     return nuevoHeroe;
 }
-
 const heroeNombre = returnName();
 let otroNombre = returnName();
 console.log("De que tipo es", typeof otroNombre);
-
-
 //parametros obligatorios en funciones 
-const nombreCompleto= (nombre:string, apellido:string) =>{
+const nombreCompleto = (nombre, apellido) => {
     return `${nombre} ${apellido}`;
-}
-
+};
 const nombre = nombreCompleto("Peyton", "Manning");
 console.log(nombre);
-
 //parametros opcionales en funciones SWIF
-const nombreCompleto2 = (nombre:string, apellido?:string) =>{
+const nombreCompleto2 = (nombre, apellido) => {
     return `${nombre} ${apellido || 'sin apellido'}`;
-}
-
+};
 const nombre2 = nombreCompleto2("Peyton");
 console.log(nombre2);
-
 //parametros por defecto en funciones
-const nombreCompleto3 = (nombre?:string, apellido?:string, mayusculas:boolean= false) =>{
-    if(mayusculas){
+const nombreCompleto3 = (nombre, apellido, mayusculas = false) => {
+    if (mayusculas) {
         return `${nombre || 'sin nombre'} ${apellido || 'sin apellido'}`.toUpperCase();
     }
-    else{
+    else {
         return `${nombre || 'sin nombre'} ${apellido || 'sin apellido'}`;
     }
-}
-
+};
 const nombre3 = nombreCompleto3("Peyton", "Manning", true);
 console.log(nombre3);
-
 //esta funcion en ternario 
-const nombreCompleto4 = (nombre?:string, apellido?:string, mayusculas:boolean= false) =>
-    (mayusculas) ? `${nombre || 'sin nombre'} ${apellido || 'sin apellido'}`.toUpperCase() : `${nombre || 'sin nombre'} ${apellido || 'sin apellido'}`;
-
+const nombreCompleto4 = (nombre, apellido, mayusculas = false) => (mayusculas) ? `${nombre || 'sin nombre'} ${apellido || 'sin apellido'}`.toUpperCase() : `${nombre || 'sin nombre'} ${apellido || 'sin apellido'}`;
 const nombre4 = nombreCompleto4("Peyton", "Manning", true);
 console.log(nombre4);
-
 //operador Rest donde edad queda al ultimo
-const nombreCompleto5 = (nombre:string, edad:number, ...restParametros:string[]) =>{
+const nombreCompleto5 = (nombre, edad, ...restParametros) => {
     return `${nombre} ${restParametros.join(' ')}`;
-}
-
+};
 const nombre5 = nombreCompleto5("Peyton", 44, "Manning", "Hijo");
 console.log(nombre5);
-
