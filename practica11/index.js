590742def6703d3adb4fd010a5359eb9af7fb469
PeytonDenisse <dcv78658@lasalle.edu.mx>
2024-10-01 16:03:54 -0600

    cambios

--- practica11/index.js
+++ practica11/index.js
@@ -3,11 +3,11 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {    
-    res.send('Hola MUNDO con EXPRESS!');
+    res.send('Hola EXPRESS!');
 });
 
 app.get('/productos', (req, res) => {    
-    res.send('ESTOY EN PRODUCTOS!');
+    res.send('PRODUCTOS!');
 });
 
 
