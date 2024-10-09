2346f1e6c117bdc3602759759fcc0e2e7c62b48b
PeytonDenisse <dcv78658@lasalle.edu.mx>
2024-10-08 21:14:54 -0600

    todas las practicas hasta el momento

--- practica13/index.js
+++ practica13/index.js
@@ -1,4 +1,4 @@
-const fs = require('fs')
+/*const fs = require('fs')
 console.log(fs);
 
 // const leerArchivo = (err,data) => {
@@ -32,3 +32,48 @@ fs.copyFile('archivoCopia.txt', 'archivoCopia.txt', (err) => err ? console.log(e
 //mover el archivo que se crea al copiar un archivo , a la carpeta que se crea con mkdir
 fs.copyFile('archivoCopia.txt', 'nuevaCarpeta/archivoCopia.txt', (err) => err ? console.log(err) : console.log('Archivo copiado'));
 
+
+*/
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// Servir archivos estáticos (como el HTML, CSS, etc.)
+app.use(express.static('public'));
+
+// Endpoint para obtener archivos y carpetas del directorio actual
+app.get('/api/files', (req, res) => {
+    const directoryPath = path.join(__dirname);
+
+    // Leer todos los archivos y carpetas en el directorio actual
+    fs.readdir(directoryPath, { withFileTypes: true }, (err, files) => {
+        if (err) {
+            return res.status(500).json({ error: 'No se pudieron leer los archivos' });
+        }
+
+        // Mapeamos la lista de archivos y carpetas a un formato más simple
+        const filesInfo = files.map(file => ({
+            name: file.name,
+            isDirectory: file.isDirectory()
+        }));
+
+        // Enviamos la lista de archivos al cliente
+        res.json(filesInfo);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Servidor escuchando en http://localhost:${port}`);
+});
+
+
+
+
+// RETO
+// Leer todos los archivos y carpetas
+// de nuestro directorio y vamos a crear un front-end
+// que muestre todos los archivos y carpetas usando iconos para diferenciarlos
+// usar BootStrap Icons.
--- practica16/index.js
+++ practica16/index.js
@@ -1,12 +1,12 @@
 const http = require('http');
 const fs = require('fs');
 const PUERTO = 3000;
- 
+
 const server = http.createServer((req, res) => {
- 
-    res.setHeader('Content-Type','text/html ');
- 
-    //Simple Router
+
+    res.setHeader('Content-Type', 'text/html');
+
+    // Simple Router
     let ruta = './views';
     switch (req.url) {
         case '/':
@@ -21,35 +21,35 @@ const server = http.createServer((req, res) => {
             ruta += '/contact.html';
             res.statusCode = 200;
             break;
+        case '/products':
+            ruta += '/products.html';
+            res.statusCode = 200;
+            break;
+        case '/services':
+            ruta += '/services.html';
+            res.statusCode = 200;
+            break;
         default:
             ruta += '/404.html';
             res.statusCode = 404;
             break;
     }
- 
-    fs.readFile( ruta, (err, data) => {
+
+    fs.readFile(ruta, (err, data) => {
         if (err) {
-            res.writeHead(404, {'Content-Type': 'text/html'});
+            res.writeHead(404, { 'Content-Type': 'text/html' });
             return res.end('404 Not Found');
         }
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(`
-<ul>
-<li><a href="/">Inicio</a></li>
-<li><a href="/about">Acerca de</a></li>
-<li><a href="/contact">Contacto</a></li>
-</ul>
-            `);
+        res.writeHead(res.statusCode, { 'Content-Type': 'text/html' });
         res.write(data);
         return res.end();
     });
- 
 });
- 
 
 server.listen(PUERTO, () => {
-    console.log('Servidor en el puerto 3000');
+    console.log(`Servidor en el puerto ${PUERTO}`);
 });
+
  
 //Reto
 // De esta practica16 hacer un sitio 100% funcional
