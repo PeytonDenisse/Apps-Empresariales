5bc4f076e83cea1bd10d7a4e7ac68618038cbe91
PeytonDenisse <dcv78658@lasalle.edu.mx>
2024-11-04 08:20:10 -0600

    practica de mongo correccion

--- practicaMongoDB1/index.js
+++ practicaMongoDB1/index.js
@@ -1,25 +1,30 @@
 const { MongoClient } = require('mongodb');
 
-
 // Connection URI
 const uri = "mongodb+srv://Peyton:Tita1214@cluster1.jzibs.mongodb.net/";
 const client = new MongoClient(uri);
 
-
-
 // The database to use
 async function run() {
     try {
-      await client.connect();
-      const db = client.db('blog');
-      const collection = db.collection('articulos').findOne();
-  
-      // Find the first document in the collection
-      const first = await collection.findOne();
-      console.log(articulo.titulo);
+        await client.connect();
+        const db = client.db('blog');
+        const collection = db.collection('articulos');
+    
+        // Find the first document in the collection
+        const first = await collection.findOne();
+
+        if (first) {
+            console.log(first.titulo);
+        } else {
+            console.log("No se encontró ningún documento en la colección.");
+        }
+    } catch (error) {
+        console.error("Error ejecutando la consulta:", error);
     } finally {
-      // Close the database connection when finished or an error occurs
-      await client.close();
+        // Close the database connection when finished or an error occurs
+        await client.close();
     }
-  }
-  run().catch(console.error);
+}
+
+run().catch(console.error);
